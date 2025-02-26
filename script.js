1c2b98400ed8e285317c84caa1b71ef97791e559
mrcivic <161588529+Migueldalla08@users.noreply.github.com>
2025-02-26 11:42:48 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+ let palavra1 = prompt ("escreva uma palavra");
+ let palavra2 = prompt ("escreva outra palavra");
+ if (palavra1.length > palavra2.length) {
+   console.log (palavra1);
+ } else {
+  console.log (palavra2);
+ }
+        
+   
